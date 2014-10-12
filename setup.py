87d9d9679d98ce625c7f794dfdd52c9a0f2cc28e
Valentin Lorentz <progval@progval.net>
2014-10-12 13:15:36 +0200

    Update setup.py to take account of the available HTTP library.

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from setuptools import setup, find_packages
 setup(
     name='ppp_core',
     version='0.1',
-    description='Core/router of the PPP framework',
+    description='Core/router of the PPP framework. Also contains a library ' \
+                'usable by module developpers to handle the query API.',
     url='https://github.com/ProjetPP/PPP-Core',
     author='Valentin Lorentz',
     author_email='valentin.lorentz+ppp@ens-lyon.org',
@@ -13,6 +14,7 @@ setup(
     classifiers=[
         'Environment :: No Input/Output (Daemon)',
         'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
@@ -20,6 +22,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Software Development :: Libraries',
     ],
     install_requires=[
         'requests',
