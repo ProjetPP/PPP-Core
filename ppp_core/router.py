d8fb7348cd9a68045702befdcb1c354f89e7d642
Valentin Lorentz <progval@progval.net>
2014-10-19 16:16:40 +0200

    Use ppp_datamodel.communication.* objects for interacted with the request handler.

--- ppp_core/router.py
+++ ppp_core/router.py
@@ -6,37 +6,34 @@ import requests
 import operator
 import itertools
 import functools
+from ppp_datamodel.communication import Request, Response
 from .config import Config
 from .exceptions import ClientError
 
+s = lambda x:x if isinstance(x, str) else x.decode()
+
 class Router:
     def __init__(self, request):
-        self.extract_request(request)
+        self.language = request.language
+        self.tree = request.tree
         self.config = Config()
-
-    def extract_request(self, request):
-        try:
-            self.language = request['language']
-            self.tree = request['tree']
-        except KeyError:
-            raise ClientError('Missing mandatory field in request object.')
 
     def answer(self):
         # First make all requests so modules can prepare their answer
         # while we send requests to other modules
         streams = self._get_streams()
-        answers = map(lambda x:(x[0], json.loads(x[1].content)), streams)
+        answers = map(self._stream_reader, streams)
         answers = map(self._process_answers, answers)
-        answers = itertools.chain(*answers) # Flatten answers lists
+        answers = itertools.chain(*list(answers)) # Flatten answers lists
         answers = filter(bool, answers) # Eliminate None values
-        return sorted(answers, key=operator.itemgetter('pertinence'),
+        return sorted(answers, key=operator.attrgetter('pertinence'),
                       reverse=True)
 
     def _get_streams(self):
         headers = {'Content-type': 'application/json',
                    'Accept': 'application/json'}
-        payload = json.dumps({'language': self.language, 'tree': self.tree})
-        getter = functools.partial(requests.get, stream=True,
+        payload = Request(self.language, self.tree).as_json()
+        getter = functools.partial(requests.post, stream=True,
                                    headers=headers, data=payload)
         streams = []
         for module in self.config.modules:
@@ -48,17 +45,30 @@ class Router:
                 pass
         return streams
 
+    def _stream_reader(self, stream):
+        (module, stream) = stream
+        if stream.status_code != 200:
+            logging.warning('Module %s returned %d: %s' %
+                            (module, stream.status_code, stream.content))
+            return None
+        else:
+            return (module, json.loads(s(stream.content)))
+
     def _process_answers(self, t):
-        (module, answers) = t
-        return list(map(functools.partial(self._process_answer, module), answers))
+        if t:
+            (module, answers) = t
+            answers = map(Response.from_json, answers)
+            return list(map(functools.partial(self._process_answer, module), answers))
+        else:
+            return []
 
     def _process_answer(self, module, answer):
-        pertinence = answer['pertinence']
+        pertinence = answer.pertinence
         if not isinstance(pertinence, int) and \
                 not isinstance(pertinence, float) and \
                 pertinence < 0 or pertinence > 1:
             logging.warning('Module %s answered with invalid pertinence: %r' %
                     (module, pertinence))
             return None
-        answer['pertinence'] *= module.coefficient
+        answer.pertinence *= module.coefficient
         return answer
--- tests/test_http.py
+++ tests/test_http.py
@@ -1,8 +1,9 @@
 """Test HTTP capabilities of the core's frontend."""
 
 from ppp_core.tests import PPPTestCase
+from ppp_core import app
 
-class HttpTest(PPPTestCase):
+class HttpTest(PPPTestCase(app)):
     def testPostOnly(self):
         self.assertEqual(self.app.get('/', status='*').status_int, 405)
         self.assertEqual(self.app.put('/', status='*').status_int, 405)
