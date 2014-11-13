0dcc5a967962714461ed1e3dfe29868317a1807b
Valentin Lorentz <progval@progval.net>
2014-11-13 11:07:10 +0100

    Fix ppp_core.exceptions with respect to ppp_libmodule.

--- ppp_core/exceptions.py
+++ ppp_core/exceptions.py
@@ -1,11 +1,3 @@
 """Exceptions of the PPP Core."""
 
-class ClientError(Exception):
-    """Exception raised by the router for showing an error to the
-    client."""
-    pass
-
-class BadGateway(Exception):
-    """Exception raised by the router when an error from a called
-    module has been detected."""
-    pass
+from ppp_libmodule import ClientError, BadGateway
